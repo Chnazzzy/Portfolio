2da7391156c965bb0d0d378041a930d7b39df1dd
Chnazzzy <youremail@example.com>
2026-07-24 21:07:31 +0100

    Added robotic hand and video game project pages

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Lale portfolio loaded");
+// ================================
+// BOOT SCREEN
+// ================================
+
+window.addEventListener("load", () => {
+
+    const bootScreen = document.getElementById("bootScreen");
+
+    if (bootScreen) {
+
+        setTimeout(() => {
+
+            bootScreen.classList.add("hidden");
+
+        }, 2200);
+
+    }
+
+});
